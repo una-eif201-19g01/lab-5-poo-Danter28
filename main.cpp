e8807c0dfebc59705565e0d18dfaabe309f1e9db
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-09-19 23:31:03 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-//#include "Secretaria.h"
-//#include "Gerente.h" 
-//#include "Vendedor.h"
+#include "Secretaria.h"
+#include "Gerente.h" 
+#include "Vendedor.h"
+#include "Trabajador.h"
 #include<iostream>
 using std::cout;
 int main() {
-
+Trabajador* arreglo[3];
+Secretaria sec1();
+Vendedor vend1();
+Gerente ger1();
+arreglo[0]=&sec1;
+arreglo[1]=&vend1;
+arreglo[2]=&ger1;
+for(int cont=0;cont<3;cont++){
+arreglo[i]->reporte;
+}
+	
 	
 
 };
