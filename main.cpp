d4c884e5f3095c12c0b00209462dee8f587ed338
Danter28 <danter28@gmail.com>
2019-09-19 23:38:34 -0600

    .3

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 using std::cout;
 int main() {
 Trabajador* arreglo[3];
-Secretaria sec1();
-Vendedor vend1();
-Gerente ger1();
-arreglo[0]=&sec1;
-arreglo[1]=&vend1;
-arreglo[2]=&ger1;
+Secretaria sec1("001",60,1436.50,5);
+Vendedor vend1("002",62,1248.32,2,0.15,12);
+Gerente ger1("003",48,1892.68,4,25000);
+arreglo[0]= &sec1;
+arreglo[1]= &vend1;
+arreglo[2]= &ger1;
 for(int cont=0;cont<3;cont++){
-arreglo[i]->reporte;
+arreglo[cont]->reporte;
 }
 	
 	
